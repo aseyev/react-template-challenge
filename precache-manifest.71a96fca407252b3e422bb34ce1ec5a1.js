20507d345a66853d3c685b076e55fedd7d44f2fa
aseyev <aseyev.1982@gmail.com>
2020-01-16 18:03:24 +0700

    Updates

---
+++ precache-manifest.71a96fca407252b3e422bb34ce1ec5a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d348ad9e246819cfb8895a6131c2a9",
+    "url": "/react-template-challenge/index.html"
+  },
+  {
+    "revision": "6bd413a5043116d9c083",
+    "url": "/react-template-challenge/static/css/main.1d6a3a27.chunk.css"
+  },
+  {
+    "revision": "1a8316bed7ad065edb42",
+    "url": "/react-template-challenge/static/js/2.dcb5ed4e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-template-challenge/static/js/2.dcb5ed4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bd413a5043116d9c083",
+    "url": "/react-template-challenge/static/js/main.a0bee8ff.chunk.js"
+  },
+  {
+    "revision": "fe7b460ca9799f65ad19",
+    "url": "/react-template-challenge/static/js/runtime-main.2c1de777.js"
+  },
+  {
+    "revision": "eec8ba1c0be55e00312b0df71b98b254",
+    "url": "/react-template-challenge/static/media/320×240.eec8ba1c.jpg"
+  },
+  {
+    "revision": "9c3e8757044f6fd4d094f8c6186ff355",
+    "url": "/react-template-challenge/static/media/mockup-card.9c3e8757.jpg"
+  }
+]);
