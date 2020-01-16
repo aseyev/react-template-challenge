7ba7210c812188bb453b54f2c05383dce40330eb
aseyev <aseyev.1982@gmail.com>
2020-01-16 20:51:13 +0700

    fetch 2

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -5,9 +5,38 @@ import CardsList from './cardsList';
 import s from './page.module.css';
 
 export class PageWrapper extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: ""
+        };
+    }
+
+    componentDidMount() {
+        fetch("../../list.json")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.items
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
 
     render() {
+        
+        console.log(this.state)
         return <div className={s.page_wrapper}>
             <Header />
             <CardsList />
