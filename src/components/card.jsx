658e850af321e3723e72f5c352f3722acaf40f21
aseyev <aseyev.1982@gmail.com>
2020-01-22 12:56:07 +0700

    temp

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import s from './page.module.css';
-import PreloaderCircle from './common/PreloaderCircle';
 
 //Single Card 
 let Card = (props) => {
@@ -23,51 +22,18 @@ let Card = (props) => {
 
     </div>
 }
+let Picture = (props) => {
 
-const Picture = (props) => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        fetch(props.url, {
-            headers: {
-                'Content-Type': 'image/png',
-                // 'Access-Control-Allow-Origin': '*'
-            },
-            // mode: "cors",
-            // redirect: "follow"
-        })
-            .then(resp => {
-                debugger
-                if (resp.ok) {
-                    setLoading(false);
-                }
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [props.url])
-
-    return (
-        <>
-            <div className={s.img_block}>
-                {
-                    loading &&
-                    <PreloaderCircle />
-                }
-                {
-                    !loading &&
-                    <img className={s.img} src={props.url} alt="lorempixel" />
-                }
-            </div>
-        </>
-    );
+    return <div className={s.img_block}>
+        <img className={s.img} src={props.url} alt="loading" />
+        {/* <img src={user.photos.small != null ? user.photos.small : userTemplatePic} alt={user.name} /> */}
+    </div>
 }
-
 let TitleFormatted = (props) => {
     let titleLoaded = props.props;
     let titleFormat = titleLoaded;
     let maxL = 88;
-    if (titleLoaded.length > maxL) { titleFormat = titleLoaded.slice(0, maxL - 3) + '...' }
+    if (titleLoaded.length > maxL) {titleFormat = titleLoaded.slice(0, maxL-3)+'...'}
     return <div className={s.title}>
         {titleFormat}
     </div>
@@ -88,26 +54,27 @@ let TimePassed = (props) => {
                 if (counter / 7 > 1) { return "More than 1 week ago" }
                 else {
                     if (counter === 0) { return "Posted today" }
-                    else { return "Out of Date" }
+                    else {return "Out of Date" }
+                    }
                 }
             }
         }
+        return <div className={s.date_from}>
+            {periodSwitch(diffDays)}
+        </div>
     }
-    return <div className={s.date_from}>
-        {periodSwitch(diffDays)}
-    </div>
-}
-
+    
 // Forms JSX from an array of correctly colored stars
-const StarRating = (props) => {
-    const stars = [];
-    let rating = props.props
-    for (let i = 0; i < 5; i++) {
-        let starColor = () => i < rating ? s.red : s.gray;
-        stars.push(<div key={i} className={starColor()}>&#9733;</div>);
+    let StarRating = (props) => {
+        const stars = [];
+        let rating = props.props
+        for (let i = 0; i < 5; i++) {
+            let starColor = () => i < rating ? s.red : s.gray;
+            stars.push(<div className={starColor()}>&#9733;</div>);
+        }
+        return <div className={s.rating}>
+            {stars}
+        </div>
     }
-    return <div className={s.rating}>
-        {stars}
-    </div>
-}
-export default Card;
+
+    export default Card;
